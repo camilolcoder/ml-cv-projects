37854a65bbd178b884843f5fafdcad6751519c07
Camilo Colmenares <j.colmenares@uniandes.edu.co>
2024-06-08 22:15:35 -0500

    Hand points detectino working

--- hand_gesture2.py
+++ hand_gesture2.py
@@ -34,39 +34,39 @@ class handDetector():
         self.lmList = []
         if self.results.multi_hand_landmarks:
             myHand = self.results.multi_hand_landmarks[handNo]
-        for id, lm in enumerate(myHand.landmark):
-            # print(id, lm)
-            h, w, c = img.shape
-            cx, cy = int(lm.x * w), int(lm.y * h)
-            xList.append(cx)
-            yList.append(cy)
-        # print(id, cx, cy)
-        self.lmList.append([id, cx, cy])
-        if draw:
-            cv2.circle(img, (cx, cy), 5, (255, 0, 255), cv2.FILLED)
-            xmin, xmax = min(xList), max(xList)
-            ymin, ymax = min(yList), max(yList)
-            bbox = xmin, ymin, xmax, ymax
+            for id, lm in enumerate(myHand.landmark):
+                # print(id, lm)
+                h, w, c = img.shape
+                cx, cy = int(lm.x * w), int(lm.y * h)
+                xList.append(cx)
+                yList.append(cy)
+            # print(id, cx, cy)
+            self.lmList.append([id, cx, cy])
+            if draw:
+                cv2.circle(img, (cx, cy), 5, (255, 0, 255), cv2.FILLED)
+                xmin, xmax = min(xList), max(xList)
+                ymin, ymax = min(yList), max(yList)
+                bbox = xmin, ymin, xmax, ymax
 
         if draw:
-            cv2.rectangle(img, (bbox[0] – 20, bbox[1] – 20),
-        (bbox[2] + 20, bbox[3] + 20), (0, 255, 0), 2)
+            cv2.rectangle(img, (bbox[0] - 20, bbox[1] - 20),
+(bbox[2] + 20, bbox[3] + 20), (0, 255, 0), 2)
 
         return self.lmList, bbox
 
     def fingersUp(self):
         fingers = []
         # Thumb
-        if self.lmList[self.tipIds[0]][1] > self.lmList[self.tipIds[0] – 1][1]:
+        if self.lmList[self.tipIds[0]][1] > self.lmList[self.tipIds[0] - 1][1]:
             fingers.append(1)
         else:
             fingers.append(0)
         # 4 Fingers
         for id in range(1, 5):
-            if self.lmList[self.tipIds[id]][2] < self.lmList[self.tipIds[id] – 2][2]:
-            fingers.append(1)
+            if self.lmList[self.tipIds[id]][2] < self.lmList[self.tipIds[id] - 2][2]:
+                fingers.append(1)
             else:
-            fingers.append(0)
+                fingers.append(0)
         return fingers
 
     def findDistance(self, p1, p2, img, draw=True):
@@ -81,12 +81,12 @@ class handDetector():
             cv2.line(img, (x1, y1), (x2, y2), (255, 0, 255), 3)
             cv2.circle(img, (cx, cy), 15, (255, 0, 255), cv2.FILLED)
 
-            length = math.hypot(x2 – x1, y2 – y1)
+            length = math.hypot(x2 - x1, y2 - y1)
         return length, img, [x1, y1, x2, y2, cx, cy]
 
 def main():
     pTime = 0
-    cap = cv2.VideoCapture(1)
+    cap = cv2.VideoCapture(0)
     detector = handDetector()
     while True:
         success, img = cap.read()
@@ -96,7 +96,7 @@ def main():
             print(lmList[4])
 
         cTime = time.time()
-        fps = 1 / (cTime – pTime)
+        fps = 1 / (cTime - pTime)
         pTime = cTime
 
         cv2.putText(img, str(int(fps)), (10, 70), cv2.FONT_HERSHEY_PLAIN, 3,
